2e984453074999065fc752b0f450d8f2206e8018
Christine McClure <camcclure@gmail.com>
2015-06-09 21:22:04 -0500

    readd after cache

---
+++ README.md
@@ -0,0 +1,7 @@
+Include
+
+* beer on tap  
+* wine on tap
+* events
+
+https://gist.github.com/ericds/62daad831fb1fee94e0e
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("includes/head.html");?>
+
+  <body>
+    <div class="row">
+      <div class="large-12 columns">
+        <h1>Welcome to Foundation</h1>
+        <?php include("includes/nav.html"); ?>
+    </div>
+    
+  
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
