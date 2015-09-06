bb56b0d298b41018e638b74c5f911701df7ec70c
Christine McClure <camcclure@gmail.com>
2015-09-06 15:29:43 -0500

    add jquery CDN link and beermenu nav list

--- beers/index.php
+++ beers/index.php
@@ -2,12 +2,20 @@
 <title>Cary's Lounge - Beers</title>
 </head>
   <body>
-        <h1>Beer Menu</h1>
-        <?php include("../includes/nav.html"); ?>
-      <div id="menu_widget">
-        <div id="onTap">
-        </div>
+    <h1>Beer Menu</h1>
+    <?php include("../includes/nav.html"); ?>
+    <ul class="sub-nav">
+      <li id="featured">Featured Items</li>
+      <li id="beersTap">Beers On Tap</li>
+      <li id="beersBottled">Beers Bottled</li>
+      <li id="beersCanned">Beers Canned</li>
+      <li id="ciders">Cider</li>
+      <li id="wines">Wine on Tap</li>
+    </ul>
+    <div id="menu_widget">
+      <div id="onTap">
       </div>
+    </div>
       <script src="https://www.beermenus.com/menu_widgets/2827?no_links=1&beer_descriptions=1" type="text/javascript" charset="utf-8"></script>        
   </body>
 </html>
