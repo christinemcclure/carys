f3b2d8e6b8c95537fc3fc91facd623c8e9864194
Christine McClure <camcclure@gmail.com>
2015-09-06 21:52:58 -0500

    beermenus active/hide function working

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
   (function($){
     highlightOneBeerType=function(showType) {
-      var items = new Array ("featured", "on_tap", "bottles", "cans", "cider", "wine");
-      $("#beer_menu > #"+showType).addClass('active');
-      var hideItems = items.filter(showType);
+      var items = new Array ("featured", "on_tap", "bottles", "cans", "cider", "wine"); // ids from beermenus.com      
+      for (var i in items) {
+        if (items[i].indexOf(showType) > -1){
+           $("#beer_menu > #"+items[i]).addClass('active');
+        }
+        else{
+          $("#beer_menu > #"+items[i]).addClass('hide');
+        }
+      }
     }
    })($);
   
