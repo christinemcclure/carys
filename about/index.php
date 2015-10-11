0876d608b0141f053280a1ed60fab2f66ad900a0
Christine McClure <camcclure@gmail.com>
2015-10-11 13:46:06 -0500

    created about directory and page

---
+++ about/index.php
@@ -0,0 +1,16 @@
+<?php 
+include("../includes/head.html"); 
+include("../includes/functions.php"); ?>
+<title>Cary's Lounge - About</title>
+</head>  
+  <body id="pool">
+    <div class="container">
+      <?php include("../includes/header.html"); ?>
+      <div id="main">
+        <?php include("../includes/nav.html"); ?>
+        <h1>About Cary's Lounge</h1>
+      </div>
+    </div>
+
+  </body>
+</html>
