f495b5e486b8465ac6d2e006fbbfdb6c862faa20
Christine McClure <camcclure@gmail.com>
2015-10-11 13:25:36 -0500

    home page using new structure

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php include("includes/head.html");?>
 <title>Cary's Lounge</title>
 </head>
-  <body>
+  <body id="home">
+    <div class="container">
+      <?php include("includes/header.html"); ?>
+      <div id="main">
         <?php include("includes/nav.html"); ?>
-    
-
-  
+        <div class="col1">
+          Home page welcome text.
+        </div>
+        <div class="col2">
+          Weekly specials
+        </div>
+      </div>
+      <?php include("includes/footer.html"); ?>
+    </div>
   </body>
 </html>
