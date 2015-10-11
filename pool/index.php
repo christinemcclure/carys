e3746f92087b87789831a1126cf2b1a0635aac23
Christine McClure <camcclure@gmail.com>
2015-10-11 13:43:46 -0500

    created pool directory and page

---
+++ pool/index.php
@@ -0,0 +1,16 @@
+<?php 
+include("../includes/head.html"); 
+include("../includes/functions.php"); ?>
+<title>Cary's Lounge - Pool</title>
+</head>  
+  <body id="pool">
+    <div class="container">
+      <?php include("../includes/header.html"); ?>
+      <div id="main">
+        <?php include("../includes/nav.html"); ?>
+        <h1>Pool</h1>
+      </div>
+    </div>
+
+  </body>
+</html>
