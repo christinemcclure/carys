db2452a186ae3ec69be35a084a0746e24abe2c65
Christine McClure <camcclure@gmail.com>
2015-10-11 13:41:20 -0500

    events page using new structure

--- events/index.php
+++ events/index.php
@@ -1,22 +1,27 @@
-<?php include("../includes/head.html"); ?>
+<?php 
+include("../includes/head.html"); 
+include("../includes/functions.php"); ?>
 <title>Cary's Lounge - Events</title>
 </head>  
-<body>
+  <body id="events">
+    <div class="container">
+      <?php include("../includes/header.html"); ?>
+      <div id="main">
+        <?php include("../includes/nav.html"); ?>
         <h1>Upcoming Events</h1>
         <?php include("../includes/nav.html"); 
- include("../includes/functions.php");
- $calendar="iit.edu_8l0d8qd4qtfn7skmgkiu55uv58@group.calendar.google.com";
- $calendar="caryslounge@gmail.com";
- $today=get_and_format_todays_date_time();
- echo "today: " . $today;
- echo "<br/>";
+          $calendar="iit.edu_8l0d8qd4qtfn7skmgkiu55uv58@group.calendar.google.com";
+          $calendar="caryslounge@gmail.com";
+          $today=get_and_format_todays_date_time();
+          echo "today: " . $today;
+          echo "<br/>";
 
- $msg=retrieve_calendar_event($calendar);
- echo "<br/>";
- echo $msg;
- 
- 
-  ?>
+          $msg=retrieve_calendar_event($calendar);
+          echo "<br/>";
+          echo $msg;
+          ?>
+      </div>
+    </div>
 
   </body>
 </html>
