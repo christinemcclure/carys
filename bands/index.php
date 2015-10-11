df9422f2eb2e6a6d091838bcea42d8d3376dc22f
Christine McClure <camcclure@gmail.com>
2015-10-11 13:45:26 -0500

    created bands directory and page

---
+++ bands/index.php
@@ -0,0 +1,16 @@
+<?php 
+include("../includes/head.html"); 
+include("../includes/functions.php"); ?>
+<title>Cary's Lounge - Bands</title>
+</head>  
+  <body id="pool">
+    <div class="container">
+      <?php include("../includes/header.html"); ?>
+      <div id="main">
+        <?php include("../includes/nav.html"); ?>
+        <h1>Bands & Live Events</h1>
+      </div>
+    </div>
+
+  </body>
+</html>
