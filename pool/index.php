eb007eb67faa90211ac064f2bcff2823a2ca58dc
Christine McClure <camcclure@gmail.com>
2015-10-14 15:18:29 -0500

    added pool text from previous site

--- pool/index.php
+++ pool/index.php
@@ -9,6 +9,12 @@ include("../includes/functions.php"); ?>
       <div id="main">
         <?php include("../includes/nav.html"); ?>
         <h1>Pool</h1>
+          <h2>Free Pool</h2>
+          <p>Come try out your game on our <a href="http://www.diamondbilliards.com/">Diamond</a>&nbsp;9-foot Pro-Am pool table. It's the best damn table in the city.</p>
+          <h2>Pool Leagues</h2>
+          <p>The table is reserved Monday-Thursday nights&nbsp;at 7pm for league play. Teams are organized through the <a href="http://www.poolplayers.com/">American Poolplayers Association</a> (APA) and <a href="http://www.tapleague.com/">Pool's Amateur Tour</a> (TAP) leagues.</p>
+          <h3>Come out and play</h3>
+          <p>Contact the Chicago divisions of the <a href="http://chicago.apaleagues.com/">APA</a> or <a href="http://www.chicagolandtapleague.com/">TAP</a>&nbsp;leagues to join or form a team.</p>        
       </div>
       <?php include("../includes/footer.html"); ?>
     </div>
