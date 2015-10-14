c673e3053f7208662d38dc5ee87c950492c3bb84
Christine McClure <camcclure@gmail.com>
2015-10-14 15:11:34 -0500

    added band text from pervious site

--- bands/index.php
+++ bands/index.php
@@ -9,6 +9,15 @@ include("../includes/functions.php"); ?>
       <div id="main">
         <?php include("../includes/nav.html"); ?>
         <h1>Bands & Live Events</h1>
+        <p>Book your band here. Typically, bands play three sets from 9pm-midnight, but we're open to ideas.</p>
+        <p>Cary's has a decent, basic PA for pretty much any sort of performance for the space.</p>
+        <h2>Stereo PA</h2>
+        <ul>
+        <li>(2) 3-way 12" PA Speakers (300 Watts per) 2000 Watt Amp</li>
+        <li>12-channel Mixer (4 mic XLR inputs, 1/4" TRS inputs and RCA)</li>
+        <li>(2) Wireless handheld microphones with built-in effects (reverb, delay, etc.)</li>
+        </ul>
+        <p>We recommend you bring your own mics if you're really serious about vocals. We also do NOT have mic stands. The room is small enough where you generally won't want to mic guitar amps or drums. You're welcome to bring your own mixer if you need more inputs or want to run a separate monitor system, or if you're a DJ and want cross-faders. Laptops and other such devices can plug right into ours with the 1/8" connector we've got.</p>
         <?php include("../includes/footer.html"); ?>
       </div>
     </div>
