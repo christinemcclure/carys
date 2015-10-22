d9bdd2997da233fd77056942f725f6a60f0cdd82
Christine McClure <camcclure@gmail.com>
2015-10-21 20:49:03 -0500

    make table caption match nav text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   (function($){
     highlightOneBeerType=function(showType) {
-      var items = new Array ("featured", "on_tap", "bottles", "cans", "cider", "wine"); // ids from beermenus.com      
+      var items = new Array ("featured", "on_tap", "bottles", "cans", "cider", "wine"); // ids from beermenus.com     
+
       for (var i in items) {
         if (items[i].indexOf(showType) > -1){
            $("#beer_menu > #"+items[i]).addClass('active');
@@ -14,6 +15,8 @@ $(document).ready(function() {
       }
       $("#beer-sub-nav").children().removeClass("active");//don't need to cycle through. just for highlighting menu item
       $("#sub_"+showType).addClass("active");
+      var captionText = $("li#sub_"+showType).html();
+      $("#beer_menu table caption").html(captionText);
     }
    })($);
   
