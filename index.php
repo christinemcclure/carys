95aff61f2a890a17be36d0234e33a18fd21b608d
Christine McClure <camcclure@gmail.com>
2015-10-25 11:35:58 -0500

    padding and float for columns

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
       <?php include("includes/header.html"); ?>
       <div id="main">
         <?php include("includes/nav.html"); ?>
-        <div class="col1 clear">
+        <div class="clear column">
           <p>Cary's Lounge is your friendly neighborhood tap catering to all your drinking needs since 1972. No thirst is too big or too small for us here at Cary's. In addition to the fine selection of beers, wines, and spirits, Cary's features free pool, free wi-fi, and a bad-ass selection on the jukebox.</p>          
           <p>Home page text. Here is <a target="_blank" href="images/sample-layout.png">the layout</a> the site will use (Except there will be "weekly specials" instead of where the "Coming Up at Cary's" is listed.). 
           </p>
         </div>
-        <div class="col2">
+        <div class="column">
           <h2>Weekly Specials</h2>
           <?php
             $calendar="dnfn2uq4avupk716k95a5cvpdk@group.calendar.google.com";
