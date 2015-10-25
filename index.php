61d547bfbffa21a8c9333ba0e9f0186ddd9f0d09
Christine McClure <camcclure@gmail.com>
2015-10-25 13:18:10 -0500

    change infrastructure of main container to keep text below

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
   <body id="home">
     <div class="container">
       <?php include("includes/header.html"); ?>
-      <div id="main">
+      <div class="background clear">
           <?php include("includes/nav.html"); ?>
+      </div> 
         <div class="clear column">
           <p>Cary's Lounge is your friendly neighborhood tap catering to all your drinking needs since 1972. No thirst is too big or too small for us here at Cary's. In addition to the fine selection of beers, wines, and spirits, Cary's features free pool, free wi-fi, and a bad-ass selection on the jukebox.</p>          
           <p>Home page text. Here is <a target="_blank" href="images/sample-layout.png">the layout</a> the site will use (Except there will be "weekly specials" instead of where the "Coming Up at Cary's" is listed.). 
@@ -23,7 +24,6 @@
             echo $msg;          
           ?>
         </div>
-      </div>
       <?php include("includes/footer.html"); ?>
     </div>
   </body>
