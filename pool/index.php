3d77db9579fde8b07d888202d8154f5367a90bf9
Christine McClure <camcclure@gmail.com>
2015-10-28 21:56:18 -0500

    added pool text

--- pool/index.php
+++ pool/index.php
@@ -5,6 +5,7 @@ include("../includes/functions.php"); ?>
 </head>  
   <body id="pool">
     <div class="grid">
+      
       <header>
         <div class="col-3-12">
            <div class="content">
@@ -26,6 +27,7 @@ include("../includes/functions.php"); ?>
           </div>   
         </div>      
       </header>
+      
       <nav>
         <div class="col-1-5">
            <div class="content">
@@ -53,6 +55,17 @@ include("../includes/functions.php"); ?>
            </div>
         </div>        
       </nav>
+      
+      <div class="col-1-1">
+        <h1>Pool</h1>
+        <h2>Free Pool</h2>
+        <p>Come try out your game on our <a href="http://www.diamondbilliards.com/">Diamond</a>&nbsp;9-foot Pro-Am pool table. It's the best damn table in the city.</p>
+        <h2>Pool Leagues</h2>
+        <p>The table is reserved Monday-Thursday nights&nbsp;at 7pm for league play. Teams are organized through the <a href="http://www.poolplayers.com/">American Poolplayers Association</a> (APA) and <a href="http://www.tapleague.com/">Pool's Amateur Tour</a> (TAP) leagues.</p>
+        <h3>Come out and play</h3>
+        <p>Contact the Chicago divisions of the <a href="http://chicago.apaleagues.com/">APA</a> or <a href="http://www.chicagolandtapleague.com/">TAP</a>&nbsp;leagues to join or form a team.</p>          
+      </div>
+      
     </div>
   </body>
 </html>
