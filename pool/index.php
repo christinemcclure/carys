a91b3e69a0bac891ca7a746c00dc06edf7711aa0
Christine McClure <camcclure@gmail.com>
2015-10-29 17:22:17 -0500

    moved header to includes file

--- pool/index.php
+++ pool/index.php
@@ -6,27 +6,7 @@ include("../includes/functions.php"); ?>
   <body id="pool">
     <div class="grid">
       
-      <header>
-        <div class="col-3-12">
-           <div class="content">
-            <img id="cary" alt="sketched image of Cary, the bar's founder" src="../images/logo.png">
-           </div>
-        </div>
-        <div class="col-9-12">
-          <div class="col-1-1">
-            <h1>Cary's Lounge</h1>
-          </div>   
-          <div class="col-1-2">
-            <p>2251 W. Devon Ave.</p>
-            <p>Chicago, IL 60659 (<a href="http://tinyurl.com/map-to-carys">map</a>)</p> 
-            <p><a href="tel:17737435737">(773) 743-5737</a></p>
-          </div>   
-          <div class="col-1-2">
-            <p><a href="mailto:caryslounge@gmail.com">caryslounge@gmail.com</a></p>
-            <p>(On-site ATM)</p>
-          </div>   
-        </div>      
-      </header>
+      <?php include "../includes/header.html"; ?>
       
       <?php include "../includes/nav.html"; ?>
 
