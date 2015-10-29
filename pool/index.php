eb259e6aa908c79940c6e6c2a98d37f101067da2
Christine McClure <camcclure@gmail.com>
2015-10-28 21:27:09 -0500

    add sample grid with h1

--- pool/index.php
+++ pool/index.php
@@ -4,5 +4,12 @@ include("../includes/functions.php"); ?>
 <title>Cary's Lounge - Pool</title>
 </head>  
   <body id="pool">
+    <div class="grid grid-pad">
+      <div class="col-1-1">
+        <header>
+          <h1>Cary's Lounge</h1>
+        </header>
+      </div>
+    </div>
   </body>
 </html>
