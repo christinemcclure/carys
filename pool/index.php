2ad08460b0f104f1b6018b7fbf3eca102d24d731
Christine McClure <camcclure@gmail.com>
2015-10-28 21:24:19 -0500

    start by clearing out pool page (only basic text)

--- pool/index.php
+++ pool/index.php
@@ -4,19 +4,5 @@ include("../includes/functions.php"); ?>
 <title>Cary's Lounge - Pool</title>
 </head>  
   <body id="pool">
-    <div class="container">
-      <?php include("../includes/header.html"); ?>
-      <div id="main">
-        <?php include("../includes/nav.html"); ?>
-        <h1 class="clear">Pool</h1>
-          <h2>Free Pool</h2>
-          <p>Come try out your game on our <a href="http://www.diamondbilliards.com/">Diamond</a>&nbsp;9-foot Pro-Am pool table. It's the best damn table in the city.</p>
-          <h2>Pool Leagues</h2>
-          <p>The table is reserved Monday-Thursday nights&nbsp;at 7pm for league play. Teams are organized through the <a href="http://www.poolplayers.com/">American Poolplayers Association</a> (APA) and <a href="http://www.tapleague.com/">Pool's Amateur Tour</a> (TAP) leagues.</p>
-          <h3>Come out and play</h3>
-          <p>Contact the Chicago divisions of the <a href="http://chicago.apaleagues.com/">APA</a> or <a href="http://www.chicagolandtapleague.com/">TAP</a>&nbsp;leagues to join or form a team.</p>        
-      </div>
-      <?php include("../includes/footer.html"); ?>
-    </div>
   </body>
 </html>
