9e9cc0a287c6ac98369f236e05422a18113626f3
Christine McClure <camcclure@gmail.com>
2015-10-29 21:14:03 -0500

    updated events page to grid

--- events/index.php
+++ events/index.php
@@ -4,22 +4,31 @@ include("../includes/functions.php"); ?>
 <title>Cary's Lounge - Events</title>
 </head>  
   <body id="events">
-    <div class="container">
-      <?php include("../includes/header.html"); ?>
-      <div id="main">
-        <?php include("../includes/nav.html"); ?>
-        <h1 class="clear">Upcoming Events</h1>
-        <?php 
-          $calendar="caryslounge@gmail.com";
-          echo "<p>". date("l, F j") . "</p>";
-          $timeMin=format_calendarAPI_date_snippet(time()-7200); 
-          $timeMax=format_calendarAPI_date_snippet(time()+691200);    
-          $msg=get_and_format_calendar_events($calendar,3);
-          echo $msg;
-          ?>
+    
+    <div class="grid">
+      
+      <?php include "../includes/header.html"; ?>
+      
+      <?php include "../includes/nav.html"; ?>
+
+      <div class="col-1-1">
+        <div class="content">
+          <h1>Upcoming Events</h1>
+          <?php 
+            $calendar="caryslounge@gmail.com";
+            echo "<p>". date("l, F j") . "</p>";
+            $timeMin=format_calendarAPI_date_snippet(time()-7200); 
+            $timeMax=format_calendarAPI_date_snippet(time()+691200);    
+            $msg=get_and_format_calendar_events($calendar,3);
+            echo $msg;
+            ?>
+        </div>
       </div>
-      <?php include("../includes/footer.html"); ?>
-    </div>
+
+      <?php include "../includes/footer.html"; ?>
+
+    </div>    
+
   </body>
 </html>
 
