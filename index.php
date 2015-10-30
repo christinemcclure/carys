db91b93b10784bc5a564669ade3f53ae8177b398
Christine McClure <camcclure@gmail.com>
2015-10-30 14:59:50 -0500

    don't put mdash in a tag

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
           <div class="content">
             <p>Cary's Lounge is your friendly neighborhood tap catering to all your drinking needs since 1972. No thirst is too big or too small for us here at Cary's. In addition to the fine selection of beers, wines, and spirits, Cary's features free pool, free wi-fi, and a bad-ass selection on the jukebox.</p>          
             <blockquote>
-              Smack in the middle of Indo/Pak territory, this tiny German-style hideaway has been plugging away since ’72[...] Bartenders flip the TV channels back and forth between basketball games and The Simpsons, but if that doesn’t interest you, there’s free pool and a stash of frisky regulars to keep you occupied. <a href="http://www.timeout.com/chicago/bars/carys-lounge">&mdash; TimeOut Chicago</a>
+              Smack in the middle of Indo/Pak territory, this tiny German-style hideaway has been plugging away since ’72[...] Bartenders flip the TV channels back and forth between basketball games and The Simpsons, but if that doesn’t interest you, there’s free pool and a stash of frisky regulars to keep you occupied. &mdash; <a href="http://www.timeout.com/chicago/bars/carys-lounge">TimeOut Chicago</a>
             </blockquote>
             <p>More reviews at <a href="http://www.yelp.com/biz/carys-lounge-chicago">Yelp</a> and <a href="http://chicago.metromix.com/venues/mmxchi-carys-lounge-liquors-venue">Metromix</a></p>
           </div>
