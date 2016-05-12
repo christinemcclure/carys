66cdadb1d337061c53729c3725318c03398eb556
Christine McClure <camcclure@gmail.com>
2016-05-12 16:36:33 -0500

    removed beermenus javascript and added some static content for switch to manual updates

--- beers/index.php
+++ beers/index.php
@@ -9,23 +9,33 @@
       <div class="col-1-1">
         <div class="grid-pad content">
           
-          <ul class="clear" id="beer-sub-nav">
-            <li id="sub_on_tap">On Tap</li>
-            <li id="sub_bottles">Bottled Beers</li>
-            <li id="sub_cans">Canned Beers</li>
-            <li id="sub_cider">Ciders</li>
-            <li id="sub_wine">Wine On Tap</li>
-          </ul>
-          
-          
-          <div id="menu_widget">
-            <div id="onTap">
-            </div>
-          </div>
+        <ul id="beer-sub-nav" class="clear">
+        <li id="sub_on_tap" class="active">On Tap</li>
+        <li id="sub_bottles">Bottled Beers</li>
+        <li id="sub_cans">Canned Beers</li>
+        <li id="sub_cider">Ciders</li>
+        <li id="sub_wine">Wine On Tap</li>
+        </ul>
+        <h1>On Tap</h1>
+        <p>Name</p>
+        <p>ABV</p>
+        <p>Served In</p>
+        <p>Price</p>
+        <h2>Allagash White</h2>
+        <p>5.0</p>
+        <p>16oz Draft</p>
+        <p>$6</p>
+        <p>Brewed with a generous portion of wheat and spiced with coriander and Cura&ccedil;ao orange peel, this beer is fruity, refreshing and slightly cloudy in appearance.</p>
+        <h2>Ballast Point Grapefruit Sculpin</h2>
+        <p>7.0</p>
+        <p>16oz Draft</p>
+        <p>$6</p>
+        <p>Sculpin&rsquo;s unique flavor added with the tart freshness of grapefruit, perfectly complements this IPA&rsquo;s citrusy hop character.</p>
+
         </div>
       </div>
       <div class="col-1-1">
-        <div class="grid-pad content bump-left">
+        <div class="grid-pad content>
           <p>You can also follow us on <a href="https://www.beermenus.com/places/2827-cary-s-lounge">BeerMenus.com</a> and <a href="http://www.findmytap.com/">Find My Tap</a>. </p>
         </div>
       </div>
@@ -34,19 +44,5 @@
 
     </div>    
     
-<script src="https://www.beermenus.com/menu_widgets/2827?no_links=1&beer_descriptions=1" type="text/javascript" charset="utf-8"></script>
-   <script>
-      $( document ).ready(function() {
-        
-        $("#beer-sub-nav li").click(function (event) {
-          var thisItem=event.target.id.substring(4);// strip sub_ from item ID
-          console.log(thisItem);
-          highlightOneBeerType(thisItem);
-        });
-        
-        highlightOneBeerType("on_tap");
-        
-      });
-  </script>
   </body>
 </html>
