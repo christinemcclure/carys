a66e445f180b77fa73ac381585dff290a14ac9b7
Christine McClure <camcclure@gmail.com>
2016-05-25 15:58:27 -0500

    remove beermenus-specific fixes from js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   (function($){
     highlightOneBeerType=function(showType) {
-      var items = new Array ("on_tap", "bottles", "cans", "cider", "wine", "on_deck"); // ids from beermenus.com     
+      var items = new Array ("on_tap", "bottles", "cans", "cider", "wine"); // ids from beermenus.com     
 
       for (var i in items) {
         if (items[i].indexOf(showType) > -1){
@@ -19,26 +19,8 @@ $(document).ready(function() {
       var captionText = $("li#sub_"+showType).html();
       $("#beer_menu table caption").html(captionText);
       
-      //fix beermenus bug of always displaying wine
-      if (showType!=="wine"){
-       $("span#white_wine").hide();
-       $("span#red_wine").hide();
-      }
-      else{
-       $("span#white_wine").show();
-       $("span#white_wine table caption").html("Whites");
-       $("span#red_wine").show();        
-       $("span#red_wine table caption").html("Reds");
-      }
-      
     }
 
    })($);
    
-  //hide beer menus branding 
-  $("#beer_menu a:last-child").hide();
-  $( "#beer_menu" ).contents().filter(function() {
-      return this.nodeType === 3;
-    }).wrap( "<p></p>" ).end().filter( "br" ).remove();
-  $("#beer_menu p:last-child").hide();
 });
