e4a01a3a15021dac0a3046fdd7a429adac1206dc
Christine McClure <camcclure@gmail.com>
2016-06-01 10:00:46 -0500

    fixed error in js display

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
   (function($){
-    highlightOneBeerType=function(showType) {
-      var items = new Array ("on_tap", "bottles", "cans", "cider", "wine"); // ids from beermenus.com     
+    console.log('in file');
+    highlightOneBeerType=function(showType){
+      var items = new Array ("on_tap", "bottles", "cans", "cider", "wine", "on_deck"); // ids from beermenus.com     
 
       for (var i in items) {
         if (items[i].indexOf(showType) > -1){
-           $("#drinks #"+items[i]).addClass('active');
-           $("#drinks #"+items[i]).removeClass('hide');
+           $("#drinks > #"+items[i]).addClass('active');
+           $("#drinks > #"+items[i]).removeClass('hide');
         }
         else{
-           $("#drinks #"+items[i]).removeClass('active');
-          $("#drinks #"+items[i]).addClass('hide');
+           $("#drinks > #"+items[i]).removeClass('active');
+          $("#drinks > #"+items[i]).addClass('hide');
         }
       }
       $("#beer-sub-nav").children().removeClass("active");//don't need to cycle through. just for highlighting menu item
       $("#sub_"+showType).addClass("active");
-      
-    }
-
+    };
    })($);
-   
+
 });
