138c17d145bc0a8433760d78f3758f104e1c79ce
Christine McClure <camcclure@gmail.com>
2018-06-05 15:48:45 -0500

    first try with microdata from schema.org

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 ?>
 <title>Cary's Lounge</title>
 </head>
-  <body id="home">
+<body id="home">
+  <meta itemprop="naics" content="722410">
     <div class="grid">
       <?php include "includes/header.html"; ?>
       <?php include "includes/nav.html"; ?>
         <div class="col-7-12">
           <div class="grid-pad content">
-            <p>Cary's Lounge is your friendly neighborhood tap catering to all your drinking needs since 1972. No thirst is too big or too small for us here at Cary's! </p>
+            <p itemprop="description">Cary's Lounge is your friendly neighborhood tap catering to all your drinking needs since 1972. No thirst is too big or too small for us here at Cary's! </p>
             In addition to the great selection of beers (16 on draft!), fine wines on tap, and excellent cocktails, Cary's features free pool on our 9 foot Diamond table and free play for you and your teams. Plus, you can enjoy your drink on our wonderful outdoor patio! You’re gonna love it!
           </div>
         </div>  
